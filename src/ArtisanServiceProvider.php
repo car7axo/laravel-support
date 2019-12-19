e8ce7996b99351ba9abf4f2f62cad8ac29be12e8
alexandre.souza <alexandre.souza@uotz.com.br>
2019-12-19 13:42:37 -0300

    feature/commands-dev - notification

--- src/ArtisanServiceProvider.php
+++ src/ArtisanServiceProvider.php
@@ -5,6 +5,7 @@ use Car7axo\Laravel\Support\Console\Commands\ChannelMakeCommand;
 use Car7axo\Laravel\Support\Console\Commands\ConsoleMakeCommand;
 use Car7axo\Laravel\Support\Console\Commands\ControllerMakeCommand;
 use Car7axo\Laravel\Support\Console\Commands\MailMakeCommand;
+use Car7axo\Laravel\Support\Console\Commands\NotificationMakeCommand;
 use Car7axo\Laravel\Support\Console\Commands\ObserverMakeCommand;
 use Car7axo\Laravel\Support\Console\Commands\RequestMakeCommand;
 use \Illuminate\Foundation\Providers\ArtisanServiceProvider as LaravelArtisanServiceProvider;
@@ -31,5 +32,6 @@ class ArtisanServiceProvider extends LaravelArtisanServiceProvider
         'MailMake' => MailMakeCommand::class,
         'RequestMake' => RequestMakeCommand::class,
         'ChannelMake' => ChannelMakeCommand::class,
+        'NotificationMake' => NotificationMakeCommand::class
     ];
 }
